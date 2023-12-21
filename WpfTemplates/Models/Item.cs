1f9b4ef9f21e622409a6c9718f7ce1ef5cf98886
LK9T9-Sektor <LK9T9_Sektor@mail.ru>
2023-12-22 01:55:14 +0700

    Fixed reverted stack panel orientation.

--- WpfTemplates/Models/Item.cs
+++ WpfTemplates/Models/Item.cs
@@ -4,6 +4,7 @@ public class Item
 {
     public string Title { get; set; }
     public string Text { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
 
     public Item(string title, string text)
     {
